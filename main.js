6e9fb8da01841042aa2c55c1cd17e11401684c0a
dangrt5 <randy.dang05@gmail.com>
2019-03-14 10:19:48 -0700

    clear button finished and domcreate in progress

--- main.js
+++ main.js
@@ -16,22 +16,30 @@ class calorieCounter {
 
     addEventHandlers = () => {
         document.querySelector(".add-button").addEventListener("click", this.addButtonHandler);
+        document.querySelector(".clear-button").addEventListener("click", this.cancelButtonHandler);
     }
 
     addButtonHandler = () => {
         const foodName = document.querySelector(".food-name").value;
         const portionSize = document.querySelector(".portion-size").value;
         const calories = document.querySelector(".calories-input").value;
-
         const item = { foodName, portionSize, calories };
         this.foodList.push(item);
         this.calculateAverage(item);
+        this.appendToTable(item);
+    }
+
+    cancelButtonHandler = () => {
+        const inputs = document.querySelectorAll("input");
+        inputs.forEach(item => item.value = "");
     }
 
     calculateAverage = food => {
         this.totalCalories += parseInt(food.calories);
-        console.log("food list", this.foodList);
         document.querySelector(".label-default").textContent = parseInt(this.totalCalories / this.foodList.length);
     }
 
+    appendToTable = food => {
+
+    }
 }
